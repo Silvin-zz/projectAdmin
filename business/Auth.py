5d033abd647489b8855af44561ecc9f05f46dbe1
silvio bravo cado <pikino@mbp-de-silvio.lan>
2014-05-21 23:16:53 -0500

    Funcionalidades de Google Drive

--- business/Auth.py
+++ business/Auth.py
@@ -6,7 +6,16 @@ import httplib2
 
 class googleAuth():
 
+	#global variables 
+
+	service=""
+
+
+	def __init__(self):
+		self.service = self.getToken()
+
 	def getToken(self):
+
 		sourceKey	= settings.BASE_DIR + "/auth/b054caf6c564d84ff0396f4b42b7d928551adfeb-privatekey.p12"
 		f 			= file(sourceKey, 'rb')
 		key 		= f.read()
@@ -19,3 +28,23 @@ class googleAuth():
 		http = httplib2.Http()
 		http = credentials.authorize(http)
 		return build('drive', 'v2', http=http)
+
+
+	def newFolder(self, title, parentId=""):
+		resorce 	={}
+		if ("" in parentId):
+			resource = { 'title': title, "mimeType": "application/vnd.google-apps.folder" }
+		else:
+			resource = { 'title': title, "mimeType": "application/vnd.google-apps.folder" }
+
+
+		service			= self.getToken()
+		newdoc 			= service.files().insert( body=resource).execute()
+
+		return newdoc
+
+
+	def shareDocument(self, documentId, userEmail,role):
+		new_permission  = { 'value': "singleprojects@gmail.com", 'type': "user", 'role': "owner" }
+		result 			= self.service.permissions().insert( fileId= documentId, body=new_permission).execute()
+
--- principal/document.py
+++ principal/document.py
@@ -11,50 +11,12 @@ import json
 
 
 def showDocuments(request):
-	# client = gdata.docs.service.DocsService()
-	# client.ClientLogin('singleprojects@gmail.com', 's1ngl3pr0j3ct')
-	# documents_feed 	= client.GetDocumentListFeed()
+
 	token 		= googleAuth()
-	service 	=token.getToken()
-	resource = { 'title': 'Datos Ventas', "mimeType": "application/vnd.google-apps.folder" }
-	
-    resource = {
-        'title': "Datos Ventas",
-        "mimeType": "application/vnd.google-apps.folder",
-  	}
-  	
-  	
-  	newdoc = service.files().insert(
-        body=resource,
-    ).execute()
-    
-    new_permission = {
-  	    'value': "singleprojects@gmail.com",
-  	    'type': "user",
-  	    'role': "owner",
-    }
-    
-	
-	result = service.permissions().insert(
-        fileId= newdoc['id'], body=new_permission).execute()
-        
-        
-    result = service.files().list().execute()
-	
-  	
-    
-  	
-  	
-  	#result = service.files().insert(
-    #    body=resource,
-    #).execute()
-    # Respond with the new file id as JSON.
-    # self.RespondJSON(resource['id'])
+	newfolder 	= token.newFolder("Mi carpeta")
+	token.shareDocument(newfolder['id'], "singleprojects@gmail.com", "owner")    
+    #result 		= token.service.files().list().execute()
 	
 
-	#client.CreateFolder("Example",)
-	# print(client.CreateFolder("UNO"))
-	#client.CreateFolder("DOS", "Example",)
 
-
-	return render_to_response('document/show.html', {"documents":result}, context_instance=RequestContext(request))
+	return render_to_response('document/show.html', {"documents":"result"}, context_instance=RequestContext(request))
