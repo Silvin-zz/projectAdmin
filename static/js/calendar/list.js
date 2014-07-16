1354db8e5e131520bbfd90618169fba194221585
silvio bravo cado <pikino@mbp-de-silvio.lan>
2014-07-16 01:59:00 -0500

    Integrando fullcalendar

---
+++ static/js/calendar/list.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+
+
+
+/* initialize the calendar
+                 -----------------------------------------------------------------*/
+                //Date for the calendar events (dummy data)
+                var date = new Date();
+                var d = date.getDate(),
+                        m = date.getMonth(),
+                        y = date.getFullYear();
+
+                $('#calendar').fullCalendar({
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,agendaWeek,agendaDay'
+                    },
+                    buttonText: {//This is to add icons to the visible buttons
+                        prev: "<span class='fa fa-caret-left'></span>",
+                        next: "<span class='fa fa-caret-right'></span>",
+                        today: 'today',
+                        month: 'month',
+                        week: 'week',
+                        day: 'day'
+                    },
+                    firstDay: 1,
+                    allDaySlot: true,
+                    defaultView: 'agendaWeek',
+                    minTime: "08:00:00",
+                    selectable: true,
+                    selectHelper: true,
+                    axisFormat: 'HH:mm',
+					select: function(start, end) {
+					
+						
+						var title = prompt('Event Title:');
+						var eventData;
+						if (title) {
+							eventData = {
+								title: title,
+								start: start,
+								end: end
+							};
+							$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+						}
+						$('#calendar').fullCalendar('unselect');
+					},
+
+
+
+                    editable: true,
+                    droppable: true, // this allows things to be dropped onto the calendar !!!
+                    drop: function(date, allDay) { // this function is called when something is dropped
+
+                        // retrieve the dropped element's stored Event Object
+                        var originalEventObject = $(this).data('eventObject');
+
+                        // we need to copy it, so that multiple events don't have a reference to the same object
+                        var copiedEventObject = $.extend({}, originalEventObject);
+
+                        // assign it the date that was reported
+                        copiedEventObject.start = date;
+                        copiedEventObject.allDay = allDay;
+                        copiedEventObject.backgroundColor = $(this).css("background-color");
+                        copiedEventObject.borderColor = $(this).css("border-color");
+
+                        // render the event on the calendar
+                        // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                        $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+
+                        // is the "remove after drop" checkbox checked?
+                        if ($('#drop-remove').is(':checked')) {
+                            // if so, remove the element from the "Draggable Events" list
+                            $(this).remove();
+                        }
+
+                    }
+                });
+
+                
+
+});
